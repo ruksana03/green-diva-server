fbd3356e44c693642260482344747e10775c0224
Ruksana <ruksana01official@gmail.com>
2023-10-18 11:24:46 +0600

    Basic set up

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/',(req, res) => {
+    res.send('Green Diva server is running ')
+})
+
+app.listen(port, () =>{
+    console.log(`Green diva server is running on port: ${port}`)
+})
