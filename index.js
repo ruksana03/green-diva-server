913ca3d022ef12e2b8a1e41e2e919f1bc3d3719d
Ruksana <ruksana01official@gmail.com>
2023-10-18 20:43:55 +0600

    updat crud operation

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,14 +12,122 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tv4qtbo.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db("productDB").collection("products");
+
+        // post single product endpoint
+        app.post("/products", async (req, res) => {
+            const product = req.body;
+            console.log("product", product);
+            const result = await productCollection.insertOne(product);
+            console.log(result);
+            res.send(result);
+        });
+
+        // get all product endpoint 
+
+        app.get("/products", async (req, res) => {
+            const result = await productCollection.find().toArray();
+            // console.log(result);
+            res.send(result);
+        });
+
+        // delete single product end point
+        app.delete("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log("id", id);
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await productCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        });
+
+        // get single product using id endpoint
+
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log("id", id);
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await productCollection.findOne(query);
+            console.log(result);
+            res.send(result);
+        });
+
+        // update single user
+
+        app.put("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const filter = {
+                _id: new ObjectId(id),
+            };
+            const options = { upsert: true };
+            const product = {
+                $set: {
+                    img: updatedProduct.img,
+                    name: updatedProduct.name,
+                    brand : updatedProduct.brand,
+                    type : updatedProduct.type,
+                    price : updatedProduct.price,
+                    description :updatedProduct,
+                    rating :updatedProduct.rating
+                },
+            };
+            const result = await productCollection.updateOne(
+                filter,
+                product,
+                options
+            );
+            res.send(result);
+        });
 
 
 
 
-app.get('/',(req, res) => {
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Green Diva server is running ')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Green diva server is running on port: ${port}`)
 })
